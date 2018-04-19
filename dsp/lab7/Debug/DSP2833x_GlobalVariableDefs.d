6d380daaa482e7fec425c137975f678b7dc51e6c
Ali Aydin <ali.aydin@metu.edu.tr>
2018-04-19 23:33:35 +0300

    ccs ile ilk pwm kodunu yazdim

---
+++ dsp/lab7/Debug/DSP2833x_GlobalVariableDefs.d
@@ -0,0 +1,43 @@
+# FIXED
+
+DSP2833x_GlobalVariableDefs.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/source/DSP2833x_GlobalVariableDefs.c
+DSP2833x_GlobalVariableDefs.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Device.h
+DSP2833x_GlobalVariableDefs.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Adc.h
+DSP2833x_GlobalVariableDefs.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_DevEmu.h
+DSP2833x_GlobalVariableDefs.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_CpuTimers.h
+DSP2833x_GlobalVariableDefs.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_ECan.h
+DSP2833x_GlobalVariableDefs.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_ECap.h
+DSP2833x_GlobalVariableDefs.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_DMA.h
+DSP2833x_GlobalVariableDefs.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_EPwm.h
+DSP2833x_GlobalVariableDefs.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_EQep.h
+DSP2833x_GlobalVariableDefs.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Gpio.h
+DSP2833x_GlobalVariableDefs.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_I2c.h
+DSP2833x_GlobalVariableDefs.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_McBSP.h
+DSP2833x_GlobalVariableDefs.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_PieCtrl.h
+DSP2833x_GlobalVariableDefs.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_PieVect.h
+DSP2833x_GlobalVariableDefs.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Spi.h
+DSP2833x_GlobalVariableDefs.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Sci.h
+DSP2833x_GlobalVariableDefs.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_SysCtrl.h
+DSP2833x_GlobalVariableDefs.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_XIntrupt.h
+DSP2833x_GlobalVariableDefs.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Xintf.h
+
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/source/DSP2833x_GlobalVariableDefs.c: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Device.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Adc.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_DevEmu.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_CpuTimers.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_ECan.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_ECap.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_DMA.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_EPwm.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_EQep.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Gpio.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_I2c.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_McBSP.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_PieCtrl.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_PieVect.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Spi.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Sci.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_SysCtrl.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_XIntrupt.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Xintf.h: 
